e2b6c35e84b499e481a7c0b71b39d245acba6614
Kaulan Karima <125951989+kaulankarima@users.noreply.github.com>
2023-06-15 12:36:30 +0700

    Add files via upload

---
+++ Latihan Javascript/LatihanHelloWorld.js
@@ -0,0 +1,19 @@
+document.writeln("Hello World");
+//Perkenalan
+document.writeln("<p> Kaulan Karima </p>");
+document.writeln("</br>");
+document.writeln("<p> NIM 20105241009 </p>");
+document.writeln("<p> Kelas : A1 </p>");
+document.writeln(0b10101);
+document.writeln("</textarea");
+document.writeln("</br")
+
+var fullName = "Kaulan Karima";
+var firstName = "Kaulan";
+var lastName = "Karima";
+
+document.writeln("Nama Lengkap : "+ fullName);
+document.writeln("</br>");
+document.writeln("Nama Lengkap :"+ firstName);
+document.writeln("</br>")
+document.writeln("Nama Lengkap :"+ lastName);
---
+++ Latihan Javascript/script/Latihan Javascript.js
@@ -0,0 +1,6 @@
+document.writeln("Hello World");
+
+document.writeln(<p>"Nama = Kaulan Karima"</p>);
+document.writeln(<p>"Nama = Kaulan Karima"</p>);
+document.writeln(<p>"NIM = 20105241009"</p>);
+document.writeln(<p>"Kelas = TP A 2020"</p>);
